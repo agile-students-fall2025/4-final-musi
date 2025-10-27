ae474c48e7acb65afb7eb3056e2ccbc8dddd7a33
ianjiang7 <ij2184@stern.nyu.edu>
2025-10-27 16:36:37 -0400

    Added place for spotify sample for songs

--- front-end/my-app/src/pages/Music.js
+++ front-end/my-app/src/pages/Music.js
@@ -3,13 +3,14 @@ import FriendScore from "../components/FriendScore";
 import ImageHeader from "../components/ImageHeader";
 import Scores from "../components/Scores";
 import AlbumList from "../components/AlbumList"
-function Music(props) {
+function Music({musicType, artist, title, isRated}) {
     return (
         <div className="Music">
-            <ImageHeader name={props.musicName}/>
-            <Scores name={props.musicName}/>
-            {props.musicType==="Album" && <AlbumList name={props.musicName}/>}
-            <FriendScore name={props.musicName}/>
+            <ImageHeader artist={artist} title={title} isRated={isRated}/>
+            {musicType==="Song" && <SpotifySample title={title} artist={artist}/>}
+            <Scores title={title} artist={artist} isRated={isRated}/>
+            {musicType==="Album" && <AlbumList title={title} artist={artist}/>}
+            <FriendScore artist={artist} title={title}/>
             <BottomNavBar/>
         </div>
     )
