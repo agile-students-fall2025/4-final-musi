bfda8aaaedc3efea92915c733948af80608bd015
DavidYu00 <dy2262@nyu.edu>
2025-10-30 21:35:04 -0400

    properly split components css and page

--- front-end/my-app/src/pages/Leaderboard.js
+++ front-end/my-app/src/pages/Leaderboard.js
@@ -1,50 +1,4 @@
-import React from "react";
-import styled from "styled-components";
-
-const StyledLeaderboardItem = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 12px 8px;
-  border-bottom: 1px solid #f0f0f0;
-
-  &:hover {
-    background-color: #fafafa;
-  }
-`;
-
-const ItemRank = styled.span`
-  flex-basis: 40px;
-  font-size: 1rem;
-  font-weight: bold;
-  color: #888;
-`;
-
-const ItemUsername = styled.span`
-  flex-grow: 1;
-  margin-left: 16px;
-  font-size: 1.1rem;
-  font-weight: 500;
-  color: #333;
-`;
-
-const ItemScore = styled.span`
-  flex-basis: 80px;
-  text-align: right;
-  font-size: 1.1rem;
-  font-weight: bold;
-  color: #000;
-`;
-
-function LeaderboardItem({ rank, username, score }) {
-  return (
-    <StyledLeaderboardItem>
-      <ItemRank>{rank}</ItemRank>
-      <ItemUsername>{username}</ItemUsername>
-      <ItemScore>{score}</ItemScore>
-    </StyledLeaderboardItem>
-  );
-}
+import LeaderboardItem from "../components/LeaderboardItem";
 
 const leaderboardData = [
   { rank: 1, username: "@dvd", score: 1640 },
@@ -53,11 +7,11 @@ const leaderboardData = [
   { rank: 4, username: "@ian", score: 1428 },
   { rank: 5, username: "@zuhair", score: 1304 },
   { rank: 6, username: "@dvd", score: 1237 },
-  { rank: 7, username: "@soju", score: 1220 },
-  { rank: 8, username: "@oui", score: 1096 },
-  { rank: 9, username: "@beef", score: 1029 },
-  { rank: 10, username: "@eri", score: 814 },
-  { rank: 11, username: "@tofu", score: 451 },
+  { rank: 7, username: "@dvd", score: 1220 },
+  { rank: 8, username: "@dvd", score: 1096 },
+  { rank: 9, username: "@dvd", score: 1029 },
+  { rank: 10, username: "@dvd", score: 814 },
+  { rank: 11, username: "@dvd", score: 451 },
 ];
 
 function Leaderboard() {
