d9f5f33d0f13c731f8735fea06e3015c293e0e7b
DavidYu00 <dy2262@nyu.edu>
2025-12-09 19:03:13 -0500

    share song copies link to clipboard

--- front-end/my-app/src/components/ImageHeader.js
+++ front-end/my-app/src/components/ImageHeader.js
@@ -4,10 +4,24 @@ import './ImageHeader.css'
 function ImageHeader({imageUrl, title, artist, avgScore, totalRatings, isRated, onRatingClick, musicType, isBookmarked: initialBookmarked = false, spotifyId, spotifyUrl}) {
 
     const [isBookmarked, setIsBookmarked] = useState(initialBookmarked);
+    const [showToast, setShowToast] = useState(false);
 
     useEffect(() => {
       setIsBookmarked(initialBookmarked);
     }, [initialBookmarked]);
+
+    const handleShare = () => {
+      navigator.clipboard.writeText(window.location.href)
+        .then(() => {
+          setShowToast(true);
+          setTimeout(() => {
+            setShowToast(false);
+          }, 2000);
+        })
+        .catch((err) => {
+          console.error("Failed to copy link: ", err);
+        });
+    };
 
     const toggleBookmark = async () => {
       try {
@@ -67,6 +81,7 @@ function ImageHeader({imageUrl, title, artist, avgScore, totalRatings, isRated, 
       <button 
         className="image-header-share-btn"
         title="Share"
+        onClick={handleShare}
       >
         <img 
           src="/share.png" 
